5a8d13e0ae8a455ed3d75a8bbf62a2753c1232f8
dmgolembiowski <dmgolembiowski@gmail.com>
2024-01-13 19:07:31 -0500

    Many todos needed.

---
+++ chess-game/assets/do.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+magick ./pawn-black.svg ./pawn-black.png
+magick ./rook-white.svg ./rook-white.png
+magick ./pawn-white.svg ./pawn-white.png
+magick ./king-white.svg ./king-white.png
+# magick ./chess-board-regular.png ./chess-board-regular.png
+magick ./knight-black.svg ./knight-black.png
+magick ./queen-black.svg ./queen-black.png
+# magick ./chess-board-regular.svg ./chess-board-regular.png
+magick ./rook-black.svg ./rook-black.png
+magick ./bishop-white.svg ./bishop-white.png
+magick ./king-black.svg ./king-black.png
+magick ./queen-white.svg ./queen-white.png
+magick ./knight-white.svg ./knight-white.png
+magick ./bishop-black.svg ./bishop-black.png
