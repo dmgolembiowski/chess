768a5bcf347e24d33ff50b0d8aa4be671aa2dba0
dmgolembiowski <dmgolembiowski@gmail.com>
2024-01-24 19:27:49 -0500

    TODO: Functionally convert (row, col) to `chess_core::game::math::XyPair`

--- chess-game/src/main.rs
+++ chess-game/src/main.rs
@@ -12,33 +12,49 @@ use crossbeam_channel::unbounded;
 use crossbeam_utils::thread::scope;
 use raylib::prelude::*;
 
-const SQUARE_SIZE: i32 = 50;
+const SQUARE_SIZE: i32 = 80;
 
 fn main() {
     let (mut rl, thread) = raylib::init().size(1080, 720).title("Funky Chess").build();
     let mut gm = chess_core::spawn_game_master();
     let game_id: chess_core::msg::GameId = gm.create_game().unwrap();
 
+    let is_even = |pos: usize| pos % 2 == 0;
+    let is_odd = |pos: usize| !is_even(pos);
+    let tile_color = |x: usize, y: usize| {
+        if (is_odd(x) && is_odd(y)) || (is_even(x) && is_even(y)) {
+            Color::WHITE
+        } else {
+            Color::GRAY
+        }
+    };
+
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
-        let is_even = |pos: usize| pos % 2 == 0;
-        let is_odd = |pos: usize| !is_even(pos);
-        let tile_color = |x: usize, y: usize| {
-            if (is_odd(x) && is_odd(y)) || (is_even(x) && is_even(y)) {
-                Color::WHITE
-            } else {
-                Color::GRAY
-            }
-        };
         for row in 0..8 {
             for col in 0..8 {
                 let y_offset = col * SQUARE_SIZE;
                 let x_offset = row * SQUARE_SIZE;
                 let color = tile_color(row as usize, col as usize);
                 d.draw_rectangle(x_offset, y_offset, SQUARE_SIZE, SQUARE_SIZE, color);
+                // If a chess piece exists at the XyPair corresponding to the given
+                // row and column value, draw it here.
+                d.draw_text(
+                    &format!("({row}, {col})"),
+                    x_offset,
+                    y_offset,
+                    16,
+                    Color::BLACK,
+                );
             }
         }
+        // To prevent the board from being reset after every single turn
+        /*
+        'game: loop {
+
+        }
+        */
     }
 
     // Ok(())
